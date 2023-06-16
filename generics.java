de8c31b6744e5eed2d0bffdff07d58f4c7a74e98
Ashish <myasish.negi@gmail.com>
2023-06-16 14:44:30 +0530

    adding generics wildcards

---
+++ generics.java
@@ -0,0 +1,18 @@
+
+import java.util.Arrays;
+import java.util.List;
+
+public class generics{
+    public static double sumOfList( List<? extends Number> list){
+        double sum = 0.0;
+        for(Number n : list){
+            sum += n.doubleValue();
+        }
+        return sum;
+    }
+    
+    public static void main(String[] args) {
+        List<Double> l = Arrays.asList(1.12, 2.59, 64.45);
+        System.out.println("sum : " + sumOfList(l));
+    }
+}
