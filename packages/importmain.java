5828dcc9b06056714cf3dbbee018e25bc96f2e82
Ashish <myasish.negi@gmail.com>
2023-06-21 18:16:14 +0530

    added packages

---
+++ packages/importmain.java
@@ -0,0 +1,8 @@
+package packages;
+import generics.*;
+
+public class importmain {
+    public static void main(String[] args) {
+        wildcards example = new wildcards();// main method is not called implicitly
+    }
+}
