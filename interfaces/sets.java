c174179bb5be1ee0667726cd86e80d91b8f6fd34
Ashish <myasish.negi@gmail.com>
2023-08-24 23:51:21 +0530

    updated sets file

--- interfaces/sets.java
+++ interfaces/sets.java
@@ -1,5 +1,13 @@
 package interfaces;
 
+import java.util.*;
+import java.util.stream.*;
+
 public class sets {
-    
+    public static void main(String[] args) {
+        // int[] arr = {3, 4, 5, 2, 5, 5, 3, 8};
+        Set<String> distinctWords = Arrays.asList(args).stream()
+                                    .collect(Collectors.toSet());
+        System.out.println(distinctWords.size() + " Distinct words : " + distinctWords);
+    }
 }
